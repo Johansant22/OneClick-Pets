bfb3abfad635ca083c5974c960fdd53abc8a3d97
Johansant22 <johansan799@gmail.com>
2024-07-14 12:41:07 -0500

    avance de login

---
+++ resources/views/Pagina_inicio/inicio.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ asset('css/inicio.css') }}">
+
+    <title>Inicio</title>
+</head>
+<body>
+    <header>
+        <img src="imagenes/Logo.png" alt="Logo OneClick Pets" class="logo-header">
+    </header>
+<main>
+    <header>
+        <img src="imagenes/Logo.png" alt="Logo OneClick Pets" class="logo-header">
+
+        <div class="ico1">
+        <img src="imagenes/perfil.png" alt="perfil" class="perfil">
+        <img src="imagenes/carritocompras.png" alt="carritocompras" class="carrito">
+        <img src="imagenes/ajustes.png" alt="ajustes" class="ajustes">
+        </div>
+
+        <div class="b11">
+        <input type="text" placeholder="Buscar" class="barrabusqueda">
+        </div>
+
+        <nav>
+            <ul>
+              <li><a href="#">Catálogo</a></li>
+              <li><a href="#">Categoría</a></li>
+              <li><a href="#">Compras</a></li>
+              <li><a href="#">Vender</a></li>
+              <li><a href="#">Ofertas</a></li>
+              <li><a href="#">Historial</a></li>
+              <li><a href="#">Mis Favoritos</a></li>
+              <li><a href="#">Ayuda/PQRS</a></li>
+            </ul>
+          </nav>
+    </header>
+
+
+
+
+
+    <footer>
+        <div class="pie-pag">
+        <p>&copy; Copyrigth 2022 | OneClick Pets <br>Información legal | Politicas de privacidad</p>
+    </div>
+
+    <div class="redes">
+        <img src="imagenes/facebook.png" alt="Facebook" class="facebook">
+        <img src="imagenes/x.png" alt="X" class="TX">
+        <img src="imagenes/youtube.png" alt="Youtube" class="youtube">
+        <img src="imagenes/instagram.png" alt="Instagram" class="instagram">
+    </div>
+    </footer>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\InicioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::resource('inicio', InicioController::class)->names('inicio');
